e7526a98e70b8dfb01500aca1dfe4f4080644c0a
Tyshe-81 <adeniyioluwatishe@gmail.com>
2023-11-20 00:34:00 -0800

    problem

--- src/tictactoe/ButtonClickListener.java
+++ src/tictactoe/ButtonClickListener.java
@@ -1,4 +1,19 @@
 package tictactoe;
 
-public class ButtonClickListener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+public class ButtonClickListener implements ActionListener {
+    private JButton[][] buttons;
+    private String currentPlayer;
+    public ButtonClickListener(JButton[][] buttons, String currentPlayer) {
+        this.buttons = buttons;
+        this.currentPlayer = currentPlayer;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
